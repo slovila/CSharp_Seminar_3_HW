6b166dc0b00b20bbf264a97070052d78a50eecca
Alexey Nikitenko <slovila.kit51@gmail.com>
2022-06-28 08:50:26 +0300

    Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿/*
+Задача 3: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27. 
+5 -> 1, 8, 27, 64, 125
+*/
+
+System.Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+
+for (int count = 1; count <= N; count++)
+{
+    System.Console.Write(Math.Pow(count, 3) + " ");
+}
